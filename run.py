823b243e84f87b26e15983209ef080562e2ce2ef
aleksei-g <lexa_prorock@mail.ru>
2019-06-23 23:19:29 +0300

    added command-line options "count_workers"

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import argparse
 from os import getenv
 
 import aiohttp_jinja2
@@ -7,13 +8,27 @@ from aiohttp import web
 from routes import setup_routes
 from task import QueueTask
 
+
+COUNT_WORKERS_CHOISES = range(1, 6)
 DEFAULT_COUNT_WORKERS = 1
-count_workers_env = getenv('COUNT_WORKERS', '')
-COUNT_WORKERS = (
-    int(count_workers_env)
-    if count_workers_env.isdigit() and int(count_workers_env) > 0
-    else DEFAULT_COUNT_WORKERS
-)
+count_workers_env = getenv('COUNT_WORKERS', DEFAULT_COUNT_WORKERS)
+parser = argparse.ArgumentParser(description='PERX Python test')
+try:
+    parser.add_argument(
+        '--count_workers',
+        default=int(count_workers_env),
+        type=int,
+        choices=COUNT_WORKERS_CHOISES,
+        help='Number of queue workers.'
+    )
+    args = parser.parse_args()
+    COUNT_WORKERS = args.count_workers
+    if COUNT_WORKERS not in COUNT_WORKERS_CHOISES:
+        raise ValueError
+except ValueError:
+    raise ValueError(
+        'Environment variable "COUNT_WORKERS" must be an integer from interval:'
+        ' {}'.format(', '.join([str(x) for x in COUNT_WORKERS_CHOISES])))
 
 
 async def queue_worker(queue):
